949951aaf672e454a9f9b0a790d40471727825ce
yukinobu.imai <kotatu.kotatu@gmail.com>
2020-07-11 16:45:18 +0900

    Initial Commit

---
+++ BLESample WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BLESample WatchKit Extension
+//
+//  Created by 今井幸宣 on 2020/07/11.
+//  Copyright © 2020 yukinobu.imai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ BLESample WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  BLESample WatchKit Extension
+//
+//  Created by 今井幸宣 on 2020/07/11.
+//  Copyright © 2020 yukinobu.imai. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
